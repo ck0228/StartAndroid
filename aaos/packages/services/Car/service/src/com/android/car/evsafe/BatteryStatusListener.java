8c0e3a515f92c6cd44e99353e968c6817e63c310
ck0228 <51257789+ck0228@users.noreply.github.com>
2024-12-01 13:42:50 +0900

    241201-Build Complete, Execute Fail

--- aaos/packages/services/Car/service/src/com/android/car/evsafe/BatteryStatusListener.java
+++
@@ -1,5 +0,0 @@
-package com.android.car.evsafe;
-
-public interface BatteryStatusListener {
-    void onBatteryStatusChanged(int level);
-}
--- aaos/packages/services/Car/service/src/com/android/car/evsafe/GearStatusListener.java
+++
@@ -1,5 +0,0 @@
-package com.android.car.evsafe;
-
-public interface GearStatusListener {
-    void onGearStatusChanged(int gear);
-}
